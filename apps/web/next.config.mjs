659a716dc77b075e346fba90fe391c04b732ed36
DevilDogG8r <staceycbyrd@gmail.com>
2026-01-19 11:12:22 -0600

    Update next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // IMPORTANT:
-  // Railway is running `next start -p $PORT`
-  // so we MUST NOT use `output: "standalone"`.
-
+  output: "standalone",
   reactStrictMode: true,
-
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**" },
